3b7aa9a7df93baa0e36a9ab19b753a3f7bcdc52a
Robert Sanders <robert@esquimaux.org>
2009-06-24 14:56:06 -0400

    support multiple scoped_searches, and specifying columns at search time

--- lib/scoped_search.rb
+++ lib/scoped_search.rb
@@ -1,20 +1,34 @@
 module ScopedSearch
-  
-  module ClassMethods
-    
-    def self.extended(base) # :nodoc:
-      require 'scoped_search/reg_tokens'
-      require 'scoped_search/query_language_parser'
-      require 'scoped_search/query_conditions_builder'
+  class Builder
+    attr_accessor :model
+
+    def initialize(model, fields = [])
+      self.model = model
+
+      @name = :search_for
+      @fields = fields
     end
-  
-    # Creates a named scope in the class it was called upon.
-    #
-    # fields:: The fields to search on.
-    def searchable_on(*fields)
+
+    def [](name)
+      self.instance_variable_get("@#{name}")
+    end
+    
+    def name(val); @name = val; end
+    def fields(*val); @fields = val.flatten; end
+  end
+
+  class FieldSpec
+    attr_accessor :model, :scoped_search_fields, :scoped_search_assoc_groupings
+
+    def initialize(model, fields)
+      self.model = model
+      parse_fields(Array(fields).flatten.map &:to_sym)
+    end
+
+    def parse_fields(fields)
       # Make sure that the table to be searched actually exists
-      if self.table_exists?
-        
+      if model.table_exists?
+
         # Get a collection of fields to be searched on.
         if fields.first.class.to_s == 'Hash'
           if fields.first.has_key?(:only)
@@ -22,26 +36,26 @@ module ScopedSearch
             fields = fields.first[:only]
           elsif fields.first.has_key?(:except)
             # Get all the fields and remove any that are in the -except- list.
-            fields = self.column_names.collect { |column| fields.first[:except].include?(column.to_sym) ? nil : column.to_sym }.compact
+            fields = model.column_names.collect { |column| fields.first[:except].include?(column.to_sym) ? nil : column.to_sym }.compact
           end
         end
-        
+
         # Get an array of associate modules.
-        assoc_models = self.reflections.collect { |key,value| key }
-        
+        assoc_models = model.reflections.collect { |key,value| key }
+
         # Subtract out the fields to be searched on that are part of *this* model.
         # Any thing left will be associate module fields to be searched on.
-        assoc_fields = fields - self.column_names.collect { |column| column.to_sym }
-        
+        assoc_fields = fields - model.column_names.collect { |column| column.to_sym }
+
         # Subtraced out the associated fields from the fields so that you are only left
         # with fields in *this* model.
         fields -= assoc_fields
-        
+
         # Loop through each of the associate models and group accordingly each
         # associate model field to search.  Assuming the following relations:
         # has_many :clients
         # has_many :notes,
-        # belongs_to :user_type 
+        # belongs_to :user_type
         # assoc_groupings will look like
         # assoc_groupings = {:clients => [:first_name, :last_name],
         #                    :notes => [:descr],
@@ -51,57 +65,83 @@ module ScopedSearch
           assoc_groupings[assoc_model] = []
         	assoc_fields.each do |assoc_field|
         	  unless assoc_field.to_s.match(/^#{assoc_model.to_s}_/).nil?
-              assoc_groupings[assoc_model] << assoc_field.to_s.sub(/^#{assoc_model.to_s}_/, '').to_sym 
+              assoc_groupings[assoc_model] << assoc_field.to_s.sub(/^#{assoc_model.to_s}_/, '').to_sym
             end
           end
         end
-    
+
         # If a grouping does not contain any fields to be searched on then remove it.
         assoc_groupings = assoc_groupings.delete_if {|group, field_group| field_group.empty?}
-        
-        # Set the appropriate class attributes. 
-        self.cattr_accessor :scoped_search_fields, :scoped_search_assoc_groupings
+
+        # Set the appropriate class attributes.
         self.scoped_search_fields = fields
         self.scoped_search_assoc_groupings = assoc_groupings
-        self.named_scope :search_for, lambda { |keywords| self.build_scoped_search_conditions(keywords) }
       end
     end
-  
+
     # Build a hash that is used for the named_scope search_for.
     # This function will split the search_string into keywords, and search for all the keywords
     # in the fields that were provided to searchable_on.
     #
     # search_string:: The search string to parse.
-    def build_scoped_search_conditions(search_string)    
+    def build_scoped_search_conditions(search_string)
       if search_string.nil? || search_string.strip.blank?
         return {:conditions => nil}
-      else        
+      else
         query_fields = {}
-        self.scoped_search_fields.each do |field| 
-          field_name = connection.quote_table_name(table_name) + "." + connection.quote_column_name(field)
-          query_fields[field_name] = self.columns_hash[field.to_s].type
+        self.scoped_search_fields.each do |field|
+          field_name = model.connection.quote_table_name(model.table_name) + "." + model.connection.quote_column_name(field)
+          query_fields[field_name] = model.columns_hash[field.to_s].type
         end
-        
+
         assoc_model_indx = 0
         assoc_fields_indx = 1
         assoc_models_to_include = []
-        self.scoped_search_assoc_groupings.each do |group|  
+        self.scoped_search_assoc_groupings.each do |group|
           assoc_models_to_include << group[assoc_model_indx]
           group[assoc_fields_indx].each do |group_field|
-            field_name = connection.quote_table_name(group[assoc_model_indx].to_s.pluralize) + "." + connection.quote_column_name(group_field)
-            query_fields[field_name] = self.reflections[group[assoc_model_indx]].klass.columns_hash[group_field.to_s].type
+            field_name = model.connection.quote_table_name(group[assoc_model_indx].to_s.pluralize) + "." + model.connection.quote_column_name(group_field)
+            query_fields[field_name] = model.reflections[group[assoc_model_indx]].klass.columns_hash[group_field.to_s].type
           end
         end
-        
-        search_conditions = QueryLanguageParser.parse(search_string) 
-        conditions = QueryConditionsBuilder.build_query(search_conditions, query_fields) 
- 
-        retVal = {:conditions => conditions}
-        retVal[:include] = assoc_models_to_include unless assoc_models_to_include.empty?
 
-        return retVal
+        search_conditions = ScopedSearch::QueryLanguageParser.parse(search_string)
+        conditions = ScopedSearch::QueryConditionsBuilder.build_query(search_conditions, query_fields)
+
+        retval = {:conditions => conditions}
+        retval[:include] = assoc_models_to_include unless assoc_models_to_include.empty?
+
+        return retval
       end
     end
+  end
+
+  module ClassMethods
+    
+    def self.extended(base) # :nodoc:
+      require 'scoped_search/reg_tokens'
+      require 'scoped_search/query_language_parser'
+      require 'scoped_search/query_conditions_builder'
+    end
+
+    def acts_as_scoped_search(name = :scoped_search, *fields)
+      fields.size == 0 && fields = self.columns_hash.keys
+      model = self
+      built_spec = ScopedSearch::FieldSpec.new(model, fields)
+      self.named_scope name, lambda { |*args|
+        keywords = args.shift
+        spec_to_use = args.size == 0 ? built_spec : ScopedSearch::FieldSpec.new(model, args.flatten)
+        spec_to_use.build_scoped_search_conditions(keywords)
+      }
+    end
+ 
+    def searchable_on(*fields, &block)
+      fields.size == 0 && fields = self.columns_hash.keys
+      builder = Builder.new(self, fields)
+      builder.instance_eval(&block)
+      self.acts_as_scoped_search(builder['name'], builder['fields'])
+    end
+    
   end
 end
 
